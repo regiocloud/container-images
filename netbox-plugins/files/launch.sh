49425cbb321547185439a466189db92e7237e569
Marius <58997736+rhizoet@users.noreply.github.com>
2025-02-17 13:39:48 +0000

    Create static files for topology-views (#18)
    Signed-off-by: Marius <wernicke@23technologies.cloud>

---
+++ netbox-plugins/files/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# In this script, commands can be added that are to be executed
+# after the initialisation of Netbox and before the start of
+# Netbox.
+
+# The initialisation of additional objects has been outsourced to
+# a plug-in and must still be executed separately.
+if [[ $INITIALIZERS_ENABLED == "True" ]]; then
+    python3 /opt/netbox/netbox/manage.py load_initializer_data --path /opt/netbox/initializers
+fi
+
+# Collect static files.
+python3 /opt/netbox/netbox/manage.py netbox_topology_views
+python3 /opt/netbox/netbox/manage.py collectstatic --no-input
+
+exec /opt/netbox/launch-netbox.sh
